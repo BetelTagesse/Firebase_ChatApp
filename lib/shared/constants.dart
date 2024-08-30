7cfd69f97ccb22f9b12a6f5b9f8df3206117b94d
BetelTagesse <BetelTagesse367@gmail.com>
2024-08-30 15:51:26 +0300

    login page working

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:336147706957:web:fd172fbb4928ba00395804";
+  static String apiKey = "AIzaSyDeM8M6vtSO6DdKhCqB5IHutYAqXXdMuxg";
+  static String messagingSenderId = "336147706957";
+  static String projectId = "chatappflutter-8c98a";
+  final primaryColor = const Color(0xFFee7b64);
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+const textInputDecoration = InputDecoration(
+  labelStyle: TextStyle(color: Colors.black),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color(0xFFee7b64), width: 2),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color(0xFFee7b64), width: 2),
+  ),
+  errorBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color(0xFFee7b64), width: 2),
+  ),
+);
+
+void nextScreen(context, page) {
+  Navigator.push(context, MaterialPageRoute(builder: (context) => page));
+}
+
+void nextScreenReplace(context, page) {
+  Navigator.pushReplacement(
+      context, MaterialPageRoute(builder: (context) => page));
+}
